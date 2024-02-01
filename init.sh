e55ea71d1ccef6d28614decf5e79e5577b781eb6
16118 <16118@c.c>
2024-02-01 03:52:04 +0000

    240201035204

--- init.sh
+++ init.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 export LANG=en_US.UTF-8
+
+#异常终止执行函数
+trap _exit INT QUIT TERM
 #初始化函数
 initself() {
     selfversion='24.01'
@@ -221,6 +224,77 @@ initself() {
         # 去掉网卡名称后面的冒号，并输出用户选择的网卡名称
         interface=$(echo "$interface" | sed 's/://')
         echo $interface
+    }
+
+    #异常终止函数
+    _exit() {
+        if [ -e "./speedtest-cli/speedtest" ]; then
+            rm -rf ./speedtest-cli
+        fi
+        _red "\nThe script has been terminated.\n"
+
+        exit 1
+    }
+
+    #检测命令是否存在
+    _exists() {
+        local cmd="$1"
+        if eval type type >/dev/null 2>&1; then
+            eval type "$cmd" >/dev/null 2>&1
+        elif command >/dev/null 2>&1; then
+            command -v "$cmd" >/dev/null 2>&1
+        else
+            which "$cmd" >/dev/null 2>&1
+        fi
+        local rt=$?
+        return ${rt}
+    }
+
+    #检测大小
+    calc_size() {
+        local raw=$1
+        local total_size=0
+        local num=1
+        local unit="KB"
+        if ! [[ ${raw} =~ ^[0-9]+$ ]]; then
+            echo ""
+            return
+        fi
+        if [ "${raw}" -ge 1073741824 ]; then
+            num=1073741824
+            unit="TB"
+        elif [ "${raw}" -ge 1048576 ]; then
+            num=1048576
+            unit="GB"
+        elif [ "${raw}" -ge 1024 ]; then
+            num=1024
+            unit="MB"
+        elif [ "${raw}" -eq 0 ]; then
+            echo "${total_size}"
+            return
+        fi
+        total_size=$(awk 'BEGIN{printf "%.1f", '"$raw"' / '$num'}')
+        echo "${total_size} ${unit}"
+    }
+
+    to_kibyte() {
+        local raw=$1
+        awk 'BEGIN{printf "%.0f", '"$raw"' / 1024}'
+    }
+
+    calc_sum() {
+        local arr=("$@")
+        local s
+        s=0
+        for i in "${arr[@]}"; do
+            s=$((s + i))
+        done
+        echo ${s}
+    }
+    get_opsy() {
+        [ -f /etc/redhat-release ] && awk '{print $0}' /etc/redhat-release && return
+        [ -f /etc/os-release ] && awk -F'[= "]' '/PRETTY_NAME/{print $3,$4,$5}' /etc/os-release && return
+        [ -f /etc/lsb-release ] && awk -F'[="]+' '/DESCRIPTION/{print $2}' /etc/lsb-release && return
     }
 
     clear
@@ -486,9 +560,38 @@ networktools() {
         route -n
         jumpfun "--监听端口--" 0.04
         netstat -tunlp
+        jumpfun "--test IPv4/IPv6..." 0.01
+        [[ -n ${local_curl} ]] && ip_check_cmd="curl -s -m 4" || ip_check_cmd="wget -qO- -T 4"
+        ipv4_check=$( (ping -4 -c 1 -W 4 ipv4.google.com >/dev/null 2>&1 && echo true) || ${ip_check_cmd} -4 icanhazip.com 2>/dev/null)
+        ipv6_check=$( (ping -6 -c 1 -W 4 ipv6.google.com >/dev/null 2>&1 && echo true) || ${ip_check_cmd} -6 icanhazip.com 2>/dev/null)
+        if [[ -z "$ipv4_check" && -z "$ipv6_check" ]]; then
+            _yellow "Warning: Both IPv4 and IPv6 connectivity were not detected.\n"
+        fi
+        [[ -z "$ipv4_check" ]] && online="$(_red "Offline")" || online="$(_green "Online")"
+        [[ -z "$ipv6_check" ]] && online+=" / $(_red "Offline")" || online+=" / $(_green "Online")"
+
+        echo "IPv4/IPv6          : $online"
         jumpfun "--公网IP--" 0.04
         curl cip.cc
         echo
+        jumpfun "--ip地区--" 0.04
+        local org city country region
+        org="$(wget -q -T10 -O- ipinfo.io/org)"
+        city="$(wget -q -T10 -O- ipinfo.io/city)"
+        country="$(wget -q -T10 -O- ipinfo.io/country)"
+        region="$(wget -q -T10 -O- ipinfo.io/region)"
+        if [[ -n "${org}" ]]; then
+            echo "Organization       : $(_blue "${org}")"
+        fi
+        if [[ -n "${city}" && -n "${country}" ]]; then
+            echo "Location           : $(_blue "${city} / ${country}")"
+        fi
+        if [[ -n "${region}" ]]; then
+            echo "Region             : $(_yellow "${region}")"
+        fi
+        if [[ -z "${org}" ]]; then
+            echo "Region             : $(_red "No ISP detected")"
+        fi
         jumpfun "--IP连接数--" 0.04
         echo '   数量 ip'
         netstat -na | grep ESTABLISHED | awk '{print$5}' | awk -F : '{print$1}' | sort | uniq -c | sort -r
@@ -601,9 +704,81 @@ networktools() {
         sanwang() {
             bash <(curl -Lso- https://down.wangchao.info/sh/superspeed.sh)
         }
+        #多地区测速
+        netfast3() {
+
+            if [ ! -e "./speedtest-cli/speedtest" ]; then
+                sys_bit=""
+                local sysarch
+                sysarch="$(uname -m)"
+                if [ "${sysarch}" = "unknown" ] || [ "${sysarch}" = "" ]; then
+                    sysarch="$(arch)"
+                fi
+                if [ "${sysarch}" = "x86_64" ]; then
+                    sys_bit="x86_64"
+                fi
+                if [ "${sysarch}" = "i386" ] || [ "${sysarch}" = "i686" ]; then
+                    sys_bit="i386"
+                fi
+                if [ "${sysarch}" = "armv8" ] || [ "${sysarch}" = "armv8l" ] || [ "${sysarch}" = "aarch64" ] || [ "${sysarch}" = "arm64" ]; then
+                    sys_bit="aarch64"
+                fi
+                if [ "${sysarch}" = "armv7" ] || [ "${sysarch}" = "armv7l" ]; then
+                    sys_bit="armhf"
+                fi
+                if [ "${sysarch}" = "armv6" ]; then
+                    sys_bit="armel"
+                fi
+                [ -z "${sys_bit}" ] && _red "Error: Unsupported system architecture (${sysarch}).\n" && exit 1
+                url1="https://install.speedtest.net/app/cli/ookla-speedtest-1.2.0-linux-${sys_bit}.tgz"
+                url2="https://dl.lamp.sh/files/ookla-speedtest-1.2.0-linux-${sys_bit}.tgz"
+                if ! wget --no-check-certificate -q -T10 -O speedtest.tgz ${url1}; then
+                    if ! wget --no-check-certificate -q -T10 -O speedtest.tgz ${url2}; then
+                        _red "Error: Failed to download speedtest-cli.\n" && exit 1
+                    fi
+                fi
+                mkdir -p speedtest-cli && tar zxf speedtest.tgz -C ./speedtest-cli && chmod +x ./speedtest-cli/speedtest
+                rm -f speedtest.tgz
+            fi
+            printf "%-18s%-18s%-20s%-12s\n" " Node Name" "Upload Speed" "Download Speed" "Latency"
+
+            speed_test() {
+                local nodeName="$2"
+                if [ -z "$1" ]; then
+                    ./speedtest-cli/speedtest --progress=no --accept-license --accept-gdpr >./speedtest-cli/speedtest.log 2>&1
+                else
+                    ./speedtest-cli/speedtest --progress=no --server-id="$1" --accept-license --accept-gdpr >./speedtest-cli/speedtest.log 2>&1
+                fi
+                if [ $? -eq 0 ]; then
+                    local dl_speed up_speed latency
+                    dl_speed=$(awk '/Download/{print $3" "$4}' ./speedtest-cli/speedtest.log)
+                    up_speed=$(awk '/Upload/{print $3" "$4}' ./speedtest-cli/speedtest.log)
+                    latency=$(awk '/Latency/{print $3" "$4}' ./speedtest-cli/speedtest.log)
+                    if [[ -n "${dl_speed}" && -n "${up_speed}" && -n "${latency}" ]]; then
+                        printf "\033[0;33m%-18s\033[0;32m%-18s\033[0;31m%-20s\033[0;36m%-12s\033[0m\n" " ${nodeName}" "${up_speed}" "${dl_speed}" "${latency}"
+                    fi
+                fi
+            }
+            speed_test '' 'Speedtest.net'
+            speed_test '21541' 'Los Angeles, US'
+            speed_test '43860' 'Dallas, US'
+            speed_test '40879' 'Montreal, CA'
+            speed_test '24215' 'Paris, FR'
+            speed_test '28922' 'Amsterdam, NL'
+            speed_test '24447' 'Shanghai, CN'
+            speed_test '5530' 'Chongqing, CN'
+            speed_test '60572' 'Guangzhou, CN'
+            speed_test '32155' 'Hongkong, CN'
+            speed_test '23647' 'Mumbai, IN'
+            speed_test '13623' 'Singapore, SG'
+            speed_test '21569' 'Tokyo, JP'
+
+            rm -rf speedtest-cli
+
+        }
 
         menuname='首页/网络/外网测速'
-        options=("测速1" netfast "测速2-SpeedCLI" netfast2 "三网测速" sanwang)
+        options=("测速1" netfast "测速2-SpeedCLI" netfast2 "多地区测速" netfast3 "三网测速" sanwang)
 
         menu "${options[@]}"
 
@@ -781,40 +956,158 @@ sysset() {
     }
     #系统信息
     sysinfo() {
-        echo
-        hostname=$(uname -n)
-        system=$(cat /etc/os-release | grep "^NAME" | awk -F\" '{print $2}')
-        version=$(lsb_release -s -d)
-        codename=$(lsb_release -s -c)
+        cname=$(awk -F: '/model name/ {name=$2} END {print name}' /proc/cpuinfo | sed 's/^[ \t]*//;s/[ \t]*$//')
+        cores=$(awk -F: '/^processor/ {core++} END {print core}' /proc/cpuinfo)
+        freq=$(awk -F'[ :]' '/cpu MHz/ {print $4;exit}' /proc/cpuinfo)
         ccache=$(awk -F: '/cache size/ {cache=$2} END {print cache}' /proc/cpuinfo | sed 's/^[ \t]*//;s/[ \t]*$//')
         cpu_aes=$(grep -i 'aes' /proc/cpuinfo)
-        kernel=$(uname -r)
-        platform=$(uname -p)
-        address=$(ip addr | grep inet | grep -v "inet6" | grep -v "127.0.0.1" | awk '{ print $2; }' | tr '\n' '\t')
-        cpumodel=$(cat /proc/cpuinfo | grep name | cut -f2 -d: | uniq)
-        cpu=$(cat /proc/cpuinfo | grep 'processor' | sort | uniq | wc -l)
-        machinemodel=$(dmidecode | grep "Product Name" | sed 's/^[ \t]*//g' | tr '\n' '\t')
-        date=$(date)
-        tcpalgorithm=$(sysctl net.ipv4.tcp_congestion_control | awk -F ' ' '{print $3}')
-        jumpfun "--主机--" 0.04
-        echo "主机名:        $hostname" 0.02
-        echo "系统名称:      $system"
-        _blue "系统版本:      $version $codename"
-        echo "内核版本:      $kernel"
-        echo "系统类型:      $platform"
-        _green "CPU型号:      $cpumodel"
-        echo "CPU核数:       $cpu"
-        echo "CPU缓存:       $ccache"
-        if [ -n "$cpu_aes" ]; then
-            _blue "AES指令集:     yes"
+        cpu_virt=$(grep -Ei 'vmx|svm' /proc/cpuinfo)
+        tram=$(
+
+            free | awk '/Mem/ {print $2}'
+        )
+        tram=$(calc_size "$tram")
+        uram=$(
+
+            free | awk '/Mem/ {print $3}'
+        )
+        uram=$(calc_size "$uram")
+        swap=$(
+
+            free | awk '/Swap/ {print $2}'
+        )
+        swap=$(calc_size "$swap")
+        uswap=$(
+
+            free | awk '/Swap/ {print $3}'
+        )
+        uswap=$(calc_size "$uswap")
+        up=$(awk '{a=$1/86400;b=($1%86400)/3600;c=($1%3600)/60} {printf("%d days, %d hour %d min\n",a,b,c)}' /proc/uptime)
+        if _exists "w"; then
+            load=$(
+
+                w | head -1 | awk -F'load average:' '{print $2}' | sed 's/^[ \t]*//;s/[ \t]*$//'
+            )
+        elif _exists "uptime"; then
+            load=$(
+
+                uptime | head -1 | awk -F'load average:' '{print $2}' | sed 's/^[ \t]*//;s/[ \t]*$//'
+            )
         fi
-        echo "机器型号:      $machinemodel"
-        echo "系统时间:      $date"
-        echo "本机IP地址:    $address"
-        _blue "拥塞控制:      $tcpalgorithm"
+        opsy=$(get_opsy)
+        arch=$(uname -m)
+        if _exists "getconf"; then
+            lbit=$(getconf LONG_BIT)
+        else
+            echo "${arch}" | grep -q "64" && lbit="64" || lbit="32"
+        fi
+        kern=$(uname -r)
+        in_kernel_no_swap_total_size=$(
+
+            df -t simfs -t ext2 -t ext3 -t ext4 -t btrfs -t xfs -t vfat -t ntfs --total 2>/dev/null | grep total | awk '{ print $2 }'
+        )
+        swap_total_size=$(free -k | grep Swap | awk '{print $2}')
+        zfs_total_size=$(to_kibyte "$(calc_sum "$(zpool list -o size -Hp 2>/dev/null)")")
+        disk_total_size=$(calc_size $((swap_total_size + in_kernel_no_swap_total_size + zfs_total_size)))
+        in_kernel_no_swap_used_size=$(
+
+            df -t simfs -t ext2 -t ext3 -t ext4 -t btrfs -t xfs -t vfat -t ntfs --total 2>/dev/null | grep total | awk '{ print $3 }'
+        )
+        swap_used_size=$(free -k | grep Swap | awk '{print $3}')
+        zfs_used_size=$(to_kibyte "$(calc_sum "$(zpool list -o allocated -Hp 2>/dev/null)")")
+        disk_used_size=$(calc_size $((swap_used_size + in_kernel_no_swap_used_size + zfs_used_size)))
+        tcpctrl=$(sysctl net.ipv4.tcp_congestion_control | awk -F ' ' '{print $3}')
+
+        _exists "dmesg" && virtualx="$(dmesg 2>/dev/null)"
+        if _exists "dmidecode"; then
+            sys_manu="$(dmidecode -s system-manufacturer 2>/dev/null)"
+            sys_product="$(dmidecode -s system-product-name 2>/dev/null)"
+            sys_ver="$(dmidecode -s system-version 2>/dev/null)"
+        else
+            sys_manu=""
+            sys_product=""
+            sys_ver=""
+        fi
+        if grep -qa docker /proc/1/cgroup; then
+            virt="Docker"
+        elif grep -qa lxc /proc/1/cgroup; then
+            virt="LXC"
+        elif grep -qa container=lxc /proc/1/environ; then
+            virt="LXC"
+        elif [[ -f /proc/user_beancounters ]]; then
+            virt="OpenVZ"
+        elif [[ "${virtualx}" == *kvm-clock* ]]; then
+            virt="KVM"
+        elif [[ "${sys_product}" == *KVM* ]]; then
+            virt="KVM"
+        elif [[ "${cname}" == *KVM* ]]; then
+            virt="KVM"
+        elif [[ "${cname}" == *QEMU* ]]; then
+            virt="KVM"
+        elif [[ "${virtualx}" == *"VMware Virtual Platform"* ]]; then
+            virt="VMware"
+        elif [[ "${sys_product}" == *"VMware Virtual Platform"* ]]; then
+            virt="VMware"
+        elif [[ "${virtualx}" == *"Parallels Software International"* ]]; then
+            virt="Parallels"
+        elif [[ "${virtualx}" == *VirtualBox* ]]; then
+            virt="VirtualBox"
+        elif [[ -e /proc/xen ]]; then
+            if grep -q "control_d" "/proc/xen/capabilities" 2>/dev/null; then
+                virt="Xen-Dom0"
+            else
+                virt="Xen-DomU"
+            fi
+        elif [ -f "/sys/hypervisor/type" ] && grep -q "xen" "/sys/hypervisor/type"; then
+            virt="Xen"
+        elif [[ "${sys_manu}" == *"Microsoft Corporation"* ]]; then
+            if [[ "${sys_product}" == *"Virtual Machine"* ]]; then
+                if [[ "${sys_ver}" == *"7.0"* || "${sys_ver}" == *"Hyper-V" ]]; then
+                    virt="Hyper-V"
+                else
+                    virt="Microsoft Virtual Machine"
+                fi
+            fi
+        else
+            virt="Dedicated"
+        fi
+
         echo
-        jumpfun "--磁盘--" 0.04
-        df -h
+        jumpfun "--类型---------------信息--" 0.04
+        if [ -n "$cname" ]; then
+            echo " CPU Model          : $(_blue "$cname")"
+        else
+            echo " CPU Model          : $(_blue "CPU model not detected")"
+        fi
+        if [ -n "$freq" ]; then
+            echo " CPU Cores          : $(_blue "$cores @ $freq MHz")"
+        else
+            echo " CPU Cores          : $(_blue "$cores")"
+        fi
+        if [ -n "$ccache" ]; then
+            echo " CPU Cache          : $(_blue "$(calc_size $ccache)")"
+        fi
+        if [ -n "$cpu_aes" ]; then
+            echo " AES-NI             : $(_green "Enabled")"
+        else
+            echo " AES-NI             : $(_red "Disabled")"
+        fi
+        if [ -n "$cpu_virt" ]; then
+            echo " VM-x/AMD-V         : $(_green "Enabled")"
+        else
+            echo " VM-x/AMD-V         : $(_red "Disabled")"
+        fi
+        echo " Total Disk         : $(_yellow "$disk_total_size") $(_blue "($disk_used_size Used)")"
+        echo " Total Mem          : $(_yellow "$tram") $(_blue "($uram Used)")"
+        if [ "$swap" != "0" ]; then
+            echo " Total Swap         : $(_blue "$swap ($uswap Used)")"
+        fi
+        echo " System uptime      : $(_blue "$up")"
+        echo " OS                 : $(_blue "$opsy")"
+        echo " Arch               : $(_blue "$arch ($lbit Bit)")"
+        echo " Kernel             : $(_blue "$kern")"
+        echo " TCP CC             : $(_yellow "$tcpctrl")"
+        echo " Virtualization     : $(_blue "$virt")"
         nextrun
     }
     #磁盘详细信息
@@ -1027,7 +1320,7 @@ sysset() {
         iotestspeed() {
             #io测试
             io_test() {
-                (LANG=C dd if=/dev/zero of=benchtest_$$ bs=512k count=$1 conv=fdatasync && rm -f benchtest_$$) 2>&1 | awk -F, '{io=$NF} END { print io}' | sed 's/^[ \t]*//;s/[ \t]*$//'
+                (dd if=/dev/zero of=benchtest_$$ bs=512k count=$1 conv=fdatasync && rm -f benchtest_$$) 2>&1 | awk -F, '{io=$NF} END { print io}' | sed 's/^[ \t]*//;s/[ \t]*$//'
             }
             _blue "正在进行磁盘测速..."
             echo
@@ -1295,7 +1588,7 @@ ordertools() {
         #检查aria2是否已安装
         which aria2c >/dev/null 2>&1
         if [ $? == 1 ]; then
-        _blue '安装aria2..'
+            _blue '安装aria2..'
 
             apt-get install aria2
         fi
